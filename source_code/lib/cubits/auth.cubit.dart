82b154712e8b9d7647be73567b8db5d390a2f2ef
NaufalNabil617 <anaufalnabil@gmail.com>
2024-06-06 19:34:13 +0700

    edit profil benerin rute

--- source_code/lib/cubits/auth.cubit.dart
+++ source_code/lib/cubits/auth.cubit.dart
@@ -128,7 +128,7 @@ class AuthCubit extends Cubit<AuthModel> {
 
   Future<void> getProfil(int id_user) async {
     final response = await http.get(
-      Uri.parse('http://127.0.0.1:8000/profil?id_user=$id_user'),
+      Uri.parse('http://127.0.0.1:8000/profil/${id_user}'),
       headers: {
         'Authorization' : 'Bearer ${state.accessToken}',
       }
@@ -143,7 +143,7 @@ class AuthCubit extends Cubit<AuthModel> {
 
   Future<void> updateProfil(int id_user, Map<String, dynamic> profilData) async {
     final response = await http.put(
-      Uri.parse('http://127.0.0.1:8000/profil/$id_user'),
+      Uri.parse('http://127.0.0.1:8000/profil/${id_user}'),
       headers: {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ${state.accessToken}',
@@ -153,7 +153,7 @@ class AuthCubit extends Cubit<AuthModel> {
 
     if (response.statusCode == 200) {
       dataProfil = jsonDecode(response.body);
-      emit(AuthModel(userID: state.userID, accessToken: state.accessToken, error: ""));
+      getProfil(id_user);
     } else {
       throw Exception('Failed to update profil');
     }
